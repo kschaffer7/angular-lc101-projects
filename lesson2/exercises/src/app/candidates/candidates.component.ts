068d4e94eca2c538af181239b587f3f2f972884d
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-11-04 19:04:17 -0600

    all fundamental steps completed

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -22,9 +22,13 @@ export class CandidatesComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
   // Code the addToCrew function here:
-
+  addToCrew(person: object) {
+    if (!this.crew.includes(person)){
+      this.crew.push(person);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
 
